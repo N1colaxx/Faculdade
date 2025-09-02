425cdc0ec6e71491f886f72d8ee5e00f45eaa565
Nicolas <unipnicolas@gmail.com>
2025-09-02 15:25:27 -0300

    feat: Start a conexao com o BD
    criei a class q se conecta em si com o DB e seus métodos, agora conectar com as telas da View

---
+++ 3_4_semestre/4_semestre/celio/poo/Trabalhos/t02_10_09_25/src/main/java/view/InterfaceCadastro.java
@@ -0,0 +1,9 @@
+package view;
+
+public interface InterfaceCadastro {
+    public void incluir();
+    public void alterar();
+    public void consultar();
+    public void excluir();
+    public boolean validar(int operacao); //1=incluir, 2=alterar, 3=consultar, 4=excluir
+}
