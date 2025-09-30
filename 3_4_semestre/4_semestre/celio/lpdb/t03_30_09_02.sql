54a63fe35fa879c7ebb7d0974a17895e59768ac0
Nicolas Borges <unipnicolas@gmail.com>
2025-09-30 17:20:28 -0300

    Update t03_30_09_02.sql
    Resolvi fazer um FUNCTION genérica para update e esta funfando

--- 3_4_semestre/4_semestre/celio/lpdb/t03_30_09_02.sql
+++ 3_4_semestre/4_semestre/celio/lpdb/t03_30_09_02.sql
@@ -343,209 +343,7 @@ EXECUTE FUNCTION trg_log_venda_produto();
    TRIGGER para UPDATE 
    ========================================================== */
 
--- CLIENTE
-CREATE OR REPLACE FUNCTION trg_log_cliente_upd()
-RETURNS TRIGGER AS
-$$
-BEGIN
-    IF OLD.cli_limitecred IS DISTINCT FROM NEW.cli_limitecred THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','CLIENTE','CLI_LIMITECRED',
-			OLD.cli_limitecred::varchar,
-			NEW.cli_limitecred::varchar
-		);
-    END IF;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER after_update_cliente
-AFTER UPDATE ON cliente
-FOR EACH ROW
-EXECUTE FUNCTION trg_log_cliente_upd();
-
-
-
-
--- COMPRA
-CREATE OR REPLACE FUNCTION trg_log_compra_upd()
-RETURNS TRIGGER AS
-$$
-BEGIN
-	IF OLD.cpr_emissao IS DISTINCT FROM NEW.cpr_emissao THEN
-		PERFORM Func_Gravar_Log (
-			'ALTERACAO', 'COMPRA', 'CPR_EMISSAO',
-			OLD.cpr_emissao::varchar,
-			NEW.cpr_emissao::varchar
-		);
-	END IF;
-
-	IF OLD.cpr_valor IS DISTINCT FROM NEW.cpr_valor THEN 
-		PERFORM Func_Gravar_Log (
-			'ALTERACAO', 'COMPRA', 'CPR_VALOR',
-			OLD.cpr_valor::varchar,
-			NEW.cpr_valor::varchar
-		);
-	END IF;
-
-	IF OLD.cpr_desconto IS DISTINCT FROM NEW.cpr_desconto THEN 
-		PERFORM Func_Gravar_Log (
-			'ALTERACAO', 'COMPRA', 'CPR_DESCONTO',
-			OLD.cpr_desconto::varchar,
-			NEW.cpr_desconto::varchar
-		);
-	END IF;
-
-    IF OLD.cpr_total IS DISTINCT FROM NEW.cpr_total THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','COMPRA','CPR_TOTAL',
-			OLD.cpr_total::varchar,
-			NEW.cpr_total::varchar
-		);
-    END IF;
-
-	IF OLD.cpr_dtentrada IS DISTINCT FROM NEW.cpr_dtentrada THEN 
-		PERFORM Func_Gravar_Log (
-			'ALTERACAO', 'COMPRA', 'CPR_DTENTRADA',
-			OLD.cpr_dtentrada::varchar,
-			NEW.cpr_dtentrada::varchar
-		);
-	END IF;
-
-	IF OLD.cpr_obs IS DISTINCT FROM NEW.cpr_obs THEN 
-		PERFORM Func_Gravar_Log (
-			'ALTERACAO', 'COMPRA', 'CPR_OBS',
-			OLD.cpr_obs::varchar,
-			NEW.cpr_obs::varchar
-		);
-	END IF;
-
-	RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER after_update_compra
-AFTER UPDATE ON compra
-FOR EACH ROW
-EXECUTE FUNCTION trg_log_compra_upd();
-
-
-
-
--- COMPRA_PRODUTO
-CREATE OR REPLACE FUNCTION trg_log_compra_produto_upd()
-RETURNS TRIGGER AS
-$$
-BEGIN
-
-	IF OLD.pro_codigo IS DISTINCT FROM NEW.pro_codigo THEN
-        PERFORM Func_Gravar_Log(
-			'ALTERACAO','COMPRA_PRODUTO','PRO_CODIGO',
-			OLD.pro_codigo::varchar,
-			NEW.pro_codigo::varchar
-		);
-    END IF;
-
-    IF OLD.cpr_qtde IS DISTINCT FROM NEW.cpr_qtde THEN
-        PERFORM Func_Gravar_Log(
-			'ALTERACAO','COMPRA_PRODUTO','CPR_QTDE',
-			OLD.cpr_qtde::varchar,
-			NEW.cpr_qtde::varchar
-		);
-    END IF;
-
-
-    IF OLD.cpr_desconto IS DISTINCT FROM NEW.cpr_desconto THEN
-        PERFORM Func_Gravar_Log(
-			'ALTERACAO','COMPRA_PRODUTO','CPR_DESCONTO',
-			OLD.cpr_desconto::varchar,
-			NEW.cpr_desconto::varchar
-		);
-    END IF;
-
-
-    IF OLD.cpr_total IS DISTINCT FROM NEW.cpr_total THEN
-        PERFORM Func_Gravar_Log(
-			'ALTERACAO','COMPRA_PRODUTO','CPR_TOTAL',
-			OLD.cpr_total::varchar,
-			NEW.cpr_total::varchar
-		);
-    END IF;
-
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER after_update_compra_produto
-AFTER UPDATE ON compra_produto
-FOR EACH ROW
-EXECUTE FUNCTION trg_log_compra_produto_upd();
-
-
-
-
--- FORMAPAGTO
-CREATE OR REPLACE FUNCTION trg_log_formapagto_upd()
-RETURNS TRIGGER AS
-$$
-BEGIN
-    IF OLD.fpg_nome IS DISTINCT FROM NEW.fpg_nome THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','FORMAPAGTO','FPG_NOME',
-			OLD.fpg_nome,
-			NEW.fpg_nome
-		);
-    END IF;
-
-    IF OLD.fpg_ativo IS DISTINCT FROM NEW.fpg_ativo THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','FORMAPAGTO','FPG_ATIVO',
-			OLD.fpg_ativo::varchar,
-			NEW.fpg_ativo::varchar
-		);
-    END IF;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER after_update_formapagto
-AFTER UPDATE ON formapagto
-FOR EACH ROW
-EXECUTE FUNCTION trg_log_formapagto_upd();
-
-
-
-
--- FORNECEDOR
-CREATE OR REPLACE FUNCTION trg_log_fornecedor_upd()
-RETURNS TRIGGER AS
-$$
-BEGIN
-    IF OLD.for_contato IS DISTINCT FROM NEW.for_contato THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','FORNECEDOR','FOR_CONTATO',
-			OLD.for_contato,
-			NEW.for_contato
-		);
-    END IF;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER after_update_fornecedor
-AFTER UPDATE ON fornecedor
-FOR EACH ROW
-EXECUTE FUNCTION trg_log_fornecedor_upd();
-
-
-
-
--- PESSOA
-CREATE OR REPLACE FUNCTION trg_log_pessoa_upd()
+CREATE OR REPLACE FUNCTION trg_log_generic_upd()
 RETURNS TRIGGER AS
 $$
 DECLARE 
@@ -555,7 +353,7 @@ DECLARE
 BEGIN
 	-- Transformando OLD e NEW em hstores
 	FOR key, old_val, new_val IN 
-		SELECT old_kv.key, old_kv.value::varchar, new_kv.value::varchar -- subquery, que retorna apenas se houve alteração
+		SELECT old_kv.key::varchar, old_kv.value::varchar, new_kv.value::varchar -- subquery, que retorna apenas se houve alteração
 /* 
 HSTORE -> transforma a linha em uma mapa (coluna => valor) (key => value)
 EACH -> separa cada mapa em linhas individuais 
@@ -569,9 +367,9 @@ EACH -> separa cada mapa em linhas individuais
 			PERFORM Func_Gravar_Log (
 				'ALTERACAO'::varchar,
 				TG_TABLE_NAME::varchar, -- Variável especial de triggers no PostgreSQ
-				key::varchar,
-				old_val::varchar,
-				new_val::varchar
+				key,
+				old_val,
+				new_val
 			);
 		END LOOP;
 
@@ -579,271 +377,63 @@ EACH -> separa cada mapa em linhas individuais
 END;
 $$ LANGUAGE plpgsql;
 
+
+CREATE TRIGGER after_update_cliente
+AFTER UPDATE ON cliente
+FOR EACH ROW
+EXECUTE FUNCTION trg_log_generic_upd();
+
+CREATE TRIGGER after_update_compra
+AFTER UPDATE ON compra
+FOR EACH ROW
+EXECUTE FUNCTION trg_log_generic_upd();
+
+
+CREATE TRIGGER after_update_compra_produto
+AFTER UPDATE ON compra_produto
+FOR EACH ROW
+EXECUTE FUNCTION trg_log_generic_upd();
+
+CREATE TRIGGER after_update_formapagto
+AFTER UPDATE ON formapagto
+FOR EACH ROW
+EXECUTE FUNCTION trg_log_generic_upd();
+
+CREATE TRIGGER after_update_fornecedor
+AFTER UPDATE ON fornecedor
+FOR EACH ROW
+EXECUTE FUNCTION trg_log_generic_upd();
+
+
 CREATE TRIGGER after_update_pessoa
 AFTER UPDATE ON pessoa
 FOR EACH ROW
-EXECUTE FUNCTION trg_log_pessoa_upd();
-
-
-
-
--- PRODUTO	
-CREATE OR REPLACE FUNCTION trg_log_produto_upd()
-RETURNS TRIGGER AS
-$$
-DECLARE
-	key		varchar;
-	old_val varchar;
-	new_val varchar;
-BEGIN
-	FOR key, old_val, new_val IN
-		SELECT old_kv.key, old_kv.value::varchar, new_kv.value::varchar
-		FROM each(hstore(OLD)) AS old_kv
-		JOIN each(hstore(NEW)) AS new_kv ON new_kv.key = old_kv.key
-		WHERE old_kv.value IS DISTINCT FROM new_kv.value
-	LOOP
-		PERFORM Func_Gravar_Log(
-			'ALTERACAO',
-			TG_TABLE_NAME,
-			key,
-			old_val,
-			new_val
-		);
-	END LOOP;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
+EXECUTE FUNCTION trg_log_generic_upd();
 
 CREATE TRIGGER after_update_produto
 AFTER UPDATE ON produto
 FOR EACH ROW
-EXECUTE FUNCTION trg_log_produto_upd();
-
-
-
-
-
--- USUARIO
-CREATE OR REPLACE FUNCTION trg_log_usuario_upd()
-RETURNS TRIGGER AS
-$$
-BEGIN
-    IF OLD.usu_nome IS DISTINCT FROM NEW.usu_nome THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','USUARIO','USU_NOME',
-			OLD.usu_nome,
-			NEW.usu_nome
-		);
-    END IF;
-
-    IF OLD.usu_login IS DISTINCT FROM NEW.usu_login THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','USUARIO','USU_LOGIN',
-			OLD.usu_login,
-			NEW.usu_login
-		);
-    END IF;
-
-    IF OLD.usu_senha IS DISTINCT FROM NEW.usu_senha THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','USUARIO','USU_SENHA',
-			OLD.usu_senha,
-			NEW.usu_senha
-		);
-    END IF;
-
-    IF OLD.usu_cadastro IS DISTINCT FROM NEW.usu_cadastro THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','USUARIO','USU_CADASTRO',
-			OLD.usu_cadastro::varchar,
-			NEW.usu_cadastro::varchar
-		);
-    END IF;
-
-    IF OLD.usu_ativo IS DISTINCT FROM NEW.usu_ativo THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','USUARIO','USU_ATIVO',
-			OLD.usu_ativo::varchar,
-			NEW.usu_ativo::varchar
-		);
-    END IF;
-
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+EXECUTE FUNCTION trg_log_generic_upd();
 
 CREATE TRIGGER after_update_usuario
 AFTER UPDATE ON usuario
 FOR EACH ROW
-EXECUTE FUNCTION trg_log_usuario_upd();
-
-
-
--- VENDA
-CREATE OR REPLACE FUNCTION trg_log_venda_upd()
-RETURNS TRIGGER AS
-$$
-BEGIN
-
-    IF OLD.usu_codigo IS DISTINCT FROM NEW.usu_codigo THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','VENDA','USU_CODIGO',
-			OLD.usu_codigo::varchar,
-			NEW.usu_codigo::varchar);
-    END IF;
-
-    IF OLD.cli_codigo IS DISTINCT FROM NEW.cli_codigo THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','VENDA','CLI_CODIGO',
-			OLD.cli_codigo::varchar,
-			NEW.cli_codigo::varchar);
-    END IF;
-
-    IF OLD.vda_data IS DISTINCT FROM NEW.vda_data THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','VENDA','VDA_DATA',
-			OLD.vda_data::varchar,
-			NEW.vda_data::varchar);
-    END IF;
-
-    IF OLD.vda_valor IS DISTINCT FROM NEW.vda_valor THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','VENDA','VDA_VALOR',
-			OLD.vda_valor::varchar,
-			NEW.vda_valor::varchar);
-    END IF;
-
-    IF OLD.vda_desconto IS DISTINCT FROM NEW.vda_desconto THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','VENDA','VDA_DESCONTO',
-			OLD.vda_desconto::varchar,
-			NEW.vda_desconto::varchar);
-    END IF;
-
-
-    IF OLD.vda_total IS DISTINCT FROM NEW.vda_total THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','VENDA','VDA_TOTAL',
-			OLD.vda_total::varchar,
-			NEW.vda_total::varchar);
-    END IF;
-
-    IF OLD.vda_obs IS DISTINCT FROM NEW.vda_obs THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','VENDA','VDA_OBS',
-			OLD.vda_obs::varchar,
-			NEW.vda_obs::varchar);
-    END IF;
-
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+EXECUTE FUNCTION trg_log_generic_upd();
 
 CREATE TRIGGER after_update_venda
 AFTER UPDATE ON venda
 FOR EACH ROW
-EXECUTE FUNCTION trg_log_venda_upd();
-
-
-
-
--- VENDA_PAGTO
-CREATE OR REPLACE FUNCTION trg_log_venda_pagto_upd()
-RETURNS TRIGGER AS
-$$
-BEGIN
-
-    IF OLD.fpg_codigo IS DISTINCT FROM NEW.fpg_codigo THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','VENDA_PAGTO','FPG_CODIGO',
-			OLD.fpg_codigo::varchar,
-			NEW.fpg_codigo::varchar
-		);
-    END IF;
-
-    IF OLD.vdp_valor IS DISTINCT FROM NEW.vdp_valor THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','VENDA_PAGTO','VDP_VALOR',
-			OLD.vdp_valor::varchar,
-			NEW.vdp_valor::varchar
-		);
-    END IF;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+EXECUTE FUNCTION trg_log_generic_upd();
 
 CREATE TRIGGER after_update_venda_pagto
 AFTER UPDATE ON venda_pagto
 FOR EACH ROW
-EXECUTE FUNCTION trg_log_venda_pagto_upd();
-
-
-
-
-
--- VENDA_PRODUTO
-CREATE OR REPLACE FUNCTION trg_log_venda_produto_upd()
-RETURNS TRIGGER AS
-$$
-BEGIN
-
-
-    IF OLD.pro_codigo IS DISTINCT FROM NEW.pro_codigo THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','VENDA_PRODUTO','PRO_CODIGO',
-			OLD.pro_codigo::varchar,
-			NEW.pro_codigo::varchar
-		);
-    END IF;
-
-    IF OLD.vep_qtde IS DISTINCT FROM NEW.vep_qtde THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','VENDA_PRODUTO','VEP_QTDE',
-			OLD.vep_qtde::varchar,
-			NEW.vep_qtde::varchar
-		);
-    END IF;
-
-
-    IF OLD.vep_preco IS DISTINCT FROM NEW.vep_preco THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','VENDA_PRODUTO','VEP_PRECO',
-			OLD.vep_preco::varchar,
-			NEW.vep_preco::varchar
-		);
-    END IF;
-
-
-    IF OLD.vep_desconto IS DISTINCT FROM NEW.vep_desconto THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','VENDA_PRODUTO','VEP_DESCONTO',
-			OLD.vep_desconto::varchar,
-			NEW.vep_desconto::varchar
-		);
-    END IF;
-
-    IF OLD.vep_total IS DISTINCT FROM NEW.vep_total THEN
-        PERFORM Func_Gravar_Log (
-			'ALTERACAO','VENDA_PRODUTO','VEP_TOTAL',
-			OLD.vep_total::varchar,
-			NEW.vep_total::varchar
-		);
-    END IF;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+EXECUTE FUNCTION trg_log_generic_upd();
 
 CREATE TRIGGER after_update_venda_produto
 AFTER UPDATE ON venda_produto
 FOR EACH ROW
-EXECUTE FUNCTION trg_log_venda_produto_upd();
-
+EXECUTE FUNCTION trg_log_generic_upd();
 
 
 /* ==========================================================
